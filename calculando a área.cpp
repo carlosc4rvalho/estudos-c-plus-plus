5babc8808dcf1c166ad4ee7607d786aad88842f5
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2022-08-28 11:00:43 -0300

    Update and rename ex23.cpp to calculando a área.cpp

--- calculando a área.cpp
+++ calculando a área.cpp
@@ -1,6 +1,13 @@
 #include<stdio.h>
 #include<math.h>
 #include<locale.h>
+  
+
+//Faça um programa que receba do usuário o número de lados e o tamanho dos lados
+//de um polígono regular e imprima o valor da área do polígono. O programa deve
+//utilizar uma estrutura switch-case para decidir que fórmula de cálculo utilizar, de
+//acordo com o número de lados do polígono. Se o número de lados for diferente
+//de 3, 4 ou 6 o programa deve informar: “não sei calcular a área”. Áreas:
 
 int main()
 {
