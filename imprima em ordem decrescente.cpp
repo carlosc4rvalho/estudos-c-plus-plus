7e8d0fdef68cb913c210bea15ec052270ec0a59c
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2022-08-28 11:09:05 -0300

    Update imprima em ordem decrescente.cpp

--- imprima em ordem decrescente.cpp
+++ imprima em ordem decrescente.cpp
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
-int main()
-{
     //Escreva um programa em LINGUAGEM C/C++ que imprima todos os números
     // inteiros de 100 a 1 (em ordem decrescente).
+
+int main()
+{
     int i;
     for(i=100;i>0;i--)
     {
