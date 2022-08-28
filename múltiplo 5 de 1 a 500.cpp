4bbcee089c0839242cefac4db89d619c2312a3b3
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2022-08-28 10:36:47 -0300

    Update and rename ex6.cpp to múltiplo 5 de 1 a 500.cpp

--- múltiplo 5 de 1 a 500.cpp
+++ múltiplo 5 de 1 a 500.cpp
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<math.h>
 
+//Escreva um programa em LINGUAGEM C/C++ que imprima todos os números
+//múltiplos de 5, no intervalo fechado de 1 a 500.
+
 int main()
 {
     int i;
-    for(i=1; i<501; i++)
+    for(i=1; i=<500; i++)
     {
         if (i % 5 == 0)
             printf("\n multiplo por 5: %i ", i);
