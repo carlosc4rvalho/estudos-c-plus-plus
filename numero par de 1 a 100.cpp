6fb1ad8d49fd3f3844e7ec9e61cff4982427d770
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2022-08-28 10:37:48 -0300

    Update and rename ex7.cpp to numero par de 1 a 100.cpp

---
+++ numero par de 1 a 100.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    
+   // Escreva um programa em LINGUAGEM C/C++ que imprima todos os números pares
+   // do intervalo fechado de 1 a 100.
+    
+    int i;
+    for(i=1;i=<100;i++)
+    {
+        if( i % 2 == 0)
+        printf("\n %i", i);
+    }
+}
