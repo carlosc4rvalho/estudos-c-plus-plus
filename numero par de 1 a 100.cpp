a240ef40e17925e717f9a9ee7fc60b2dd0be375f
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2022-08-28 11:10:28 -0300

    Update numero par de 1 a 100.cpp

--- numero par de 1 a 100.cpp
+++ numero par de 1 a 100.cpp
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<math.h>
-int main()
-{
-    
+
    // Escreva um programa em LINGUAGEM C/C++ que imprima todos os números pares
    // do intervalo fechado de 1 a 100.
-    
+
+int main()
+{   
     int i;
     for(i=1;i=<100;i++)
     {
