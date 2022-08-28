58c383b29c74cf8ec1e79b1f59d08967dfea4517
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2022-08-28 10:43:00 -0300

    Update and rename ex9.cpp to ao quadrado de 1 a 20.cpp

--- ao quadrado de 1 a 20.cpp
+++ ao quadrado de 1 a 20.cpp
@@ -2,6 +2,9 @@
 #include<math.h>
 int main()
 {
+    
+    // Escreva um programa em LINGUAGEM C/C++ que imprima o quadrado dos números
+    // no intervalo fechado de 1 a 20.
     int i;
     for(i=1;i<21;i++)
     {
