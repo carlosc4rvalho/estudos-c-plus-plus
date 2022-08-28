49ed47d42d429a069a06f0b85939763d61be62dd
Carlos Eduardo C. Silva <110244419+CarlosC4rvalho@users.noreply.github.com>
2022-08-28 10:50:58 -0300

    Update and rename ex3.cpp to par ou impar.cpp

--- par ou impar.cpp
+++ par ou impar.cpp
@@ -4,6 +4,10 @@
 
 int main()
 {
+    
+    //Fazer um programa para ler um número do usuário e determinar se este número é
+    //par ou não par.
+    
     int num;
     setlocale(LC_ALL,"Portuguese");
     printf("\n digite um numero:  ");
